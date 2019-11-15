45b79d3238f6b7bd8c880f862f130c7b590c0b4e
Robert Knowles <robertknowles@Roberts-MacBook-Pro.local>
2019-11-15 11:43:38 -0700

    changes made

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    people: []
+  }
+  componentDidMount() {
+    axios.get('https://swapi.co/api/people').then(res => (
+      this.setState({
+        people: res.data.results
+      })
+    ))
+  }
+  render() {
+    console.log(this.state.people)
+    
+    return <div className="App">
+      {this.state.people.length ? this.state.people.map((el,i) => (
+        <div key={i}>
+          <h1>{el.name}</h1>
+          <img src={el.starships[0]} alt=""/>
+        </div>
+      )): <div>Nothing to see Here</div>}
+    </div>;
+  }
 }
 
 export default App;
